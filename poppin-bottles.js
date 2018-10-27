370f5c2ab88f65539b3aa4fa8f26ecadc2758e83
Jack Bauer <bauer.jackw@gmail.com>
2018-10-27 01:30:57 +0000

    poppin-bottles now returns an object

--- poppin-bottles.js
+++ poppin-bottles.js
@@ -3,15 +3,20 @@ poppinBottles(myArg);
 
 function poppinBottles(investment) {
   //initial purchase
+  var result = {};
   var total = Math.floor(investment / 2);
   console.log('initial total:', total);
   var fulls = total;
   var empties = 0;
   var caps = 0;
+  var totalCaps = 0;
+  var totalEmpties = 0;
 
   while(fulls > 0) {
     empties++;
+    totalEmpties++;
     caps++;
+    totalCaps++;
     fulls--;
     while(empties > 1) {
       empties -= 2;
@@ -25,10 +30,18 @@ function poppinBottles(investment) {
     }
   }
 
+  result['Total Bottles'] = total;
+  result['Remaining Bottles'] = empties;
+  result['Remaining Caps'] = caps;
+  result['Total Earned'] = {
+    'Total Empties' : totalEmpties,
+    'Total Caps' : totalCaps
+  };
+
   console.log('total: ', total);
   console.log('empties: ', empties);
   console.log('caps: ', caps);
-  return total;
+  return result;
 }
 
 module.exports = poppinBottles;
--- test/poppin-bottles-test.js
+++ test/poppin-bottles-test.js
@@ -4,18 +4,18 @@ bottles = require('../poppin-bottles');
 describe('Poopin Bottles', function() {
   it("input of 10 should have output of 15", function() {
     var expected = 15;
-    assert.isTrue(bottles(10) === 15);
+    assert.isTrue(bottles(10)['Total Bottles'] === 15);
   });
   it("input of 20 should have output of 35", function() {
     var expected = 35;
-    assert.isTrue(bottles(20) === 35);
+    assert.isTrue(bottles(20)['Total Bottles'] === 35);
   });
   it("input of 30 should have output of 55", function() {
     var expected = 55;
-    assert.isTrue(bottles(30) === 55);
+    assert.isTrue(bottles(30)['Total Bottles'] === 55);
   });
   it("input of 40 should have output of 75", function() {
     var expected = 75;
-    assert.isTrue(bottles(40) === 75);
+    assert.isTrue(bottles(40)['Total Bottles'] === 75);
   });
 });
